e1966eb3d018294e6b2f6deb13eff39a9d5930ed
Wlansford33 <wlansford27@choate.edu>
2025-04-08 14:39:17 -0400

    Homework

---
+++ Robot.py
@@ -0,0 +1,26 @@
+from tkinter.font import names
+
+from motor import Motor
+
+class Robot:
+    def __init__(self,name:str,color:str):
+        self.name = name
+        self.color = color
+        self.left_motor = Motor()
+        self.right_motor = Motor()
+
+    def drive_forward (self,speed: float):
+        self.left_motor.set_speed(speed)
+        self.right_motor.set_speed(speed)
+
+    def drive_back(self,speed: float):
+        self.left_motor.reverse()
+        self.right_motor.reverse()
+
+    def turn_left(self):
+        self.left_motor.setspeed(-1)
+        self.right_motor.set_speed(1)
+
+    def turn_right(self):
+        self.left_motor.setspeed(1)
+        self.right_motor.set_speed(-1)
